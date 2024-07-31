c819559fa39145fc4366a2db2224f33c02b8d0dc
Pablo Ruiz <pablo.ruiz.polo@gmail.com>
2024-07-31 16:42:10 +0200

    Retocado mi action del user para acceder bien a la db

--- appeleva/src/actions/user.action.js
+++ appeleva/src/actions/user.action.js
@@ -1,11 +1,11 @@
 "use server";
 
 import User from "@/app/lib/models/User";
-const connection = db.connection;
+import { connect } from "@/app/lib/dbConnect";
 
 export async function createUser(user) {
   try {
-    await connection;
+    await connect();
     const newUser = await User.create(user);
     return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
